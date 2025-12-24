94e5a46d7ca265f41b568fa2fde4c36e0c80985e
Alfa Narcisse <alfanambinintsoa@gmail.com>
2025-12-24 16:04:25 +0100

    Nettoyage des fichiers

--- src/main.c
+++ src/main.c
@@ -8,11 +8,13 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Erreur: %s", SDL_GetError());
     }
     
-    SDL_Window *win=SDL_CreateWindow("Menu",1280,720,0);
+    SDL_Window *win=SDL_CreateWindow("AWALE",1280,720,0);
     if (!win){
         fprintf(stderr, "Erreur: %s", SDL_GetError());
     }
-   
+    
+    SDL_Surface* icon = IMG_Load("../assets/images/icon.png");
+    SDL_SetWindowIcon(win,icon);
     DeviceID* deviceId = InitAudioDevice();
     AudioStreamInstance* bg_audioStreamInstance = LoadWAV("../assets/audio/ground_sound.wav", deviceId);
     SDL_SetAudioStreamGain(bg_audioStreamInstance->stream, 0.04f);
@@ -20,6 +22,7 @@ int main(int argc, char* argv[]){
     int ListePions[12] = {4,4,4,4,4,4,4,4,4,4,4,4};
     AfficheMenu(win, ListePions,bg_audioStreamInstance);
 
+    SDL_DestroySurface(icon);
     CleanUpAudioStreamInstance(bg_audioStreamInstance);
     CleanUpDevice(deviceId);
     SDL_DestroyWindow(win);
